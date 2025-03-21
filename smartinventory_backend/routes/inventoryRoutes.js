dd99336419724fdf7cb0e412677aba09cb311db8
Nita Chaudhari <116967972+Nitachaudhari@users.noreply.github.com>
2025-03-22 00:01:42 +0530

    Delete smartinventory_backend directory

--- smartinventory_backend/routes/inventoryRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { getItems, addItem } = require("../controllers/inventoryController");
-const router = express.Router();
-
-router.route("/").get(getItems).post(addItem);
-
-module.exports = router;
